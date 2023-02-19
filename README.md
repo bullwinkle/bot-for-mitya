5e459e7cedc4d1221774fce84a42d2dfb43cbc6a
Dmitriy Galnykin <developer085@gmail.com>
2023-02-19 21:37:53 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# bot-for-mitya
+# bot-for-manya
---
+++ index.ts
@@ -0,0 +1,12 @@
+import * as TelegramBot from "node-telegram-bot-api";
+
+const bot = new TelegramBot(process.env.TG_BOT_TOKEN ?? '', { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  console.log('received', msg);
+
+  bot.sendMessage(chatId, `${process.env.NODE_ENV === 'production' ? 'Remote' : 'Local'} said: ${messageText}`);
+});
