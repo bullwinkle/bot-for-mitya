c5b90d8e78d90110b6e9531e5907466cb83ad806
Dmitriy Galnykin <developer085@gmail.com>
2023-02-25 22:19:07 +0200

    images

--- src/app.ts
+++ src/app.ts
@@ -61,8 +61,8 @@ bot.on('text', async (ctx) => {
 
     await openai.createImage({
         prompt: ctx.message.text, //user entered input text will store here.
-        n: 2, //number of images that are we expecting from OpenAI API.
-        size: '512x512' //size of image that are we expecting from OpenAI API.
+        n: 1, //number of images that are we expecting from OpenAI API.
+        size: '1024x1024' //size of image that are we expecting from OpenAI API.
     }).then(x => {
         console.log('x: ', x.data);
 
